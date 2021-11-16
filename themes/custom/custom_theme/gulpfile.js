42262d67ad03d890e053a9cdebeaa8f6dcde103d
atulghate <atulghate155.com>
2021-11-16 20:42:14 +0530

    first commit

---
+++ themes/custom/custom_theme/gulpfile.js
@@ -0,0 +1,33 @@
+ const gulp = require('gulp');
+ const sass = require('gulp-sass')(require('sass'));
+ var uglifycss = require('gulp-uglifycss');
+
+gulp.task('sass', () => {
+    return gulp.src('sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('css/'))
+})
+gulp.task('message', async function () {
+    return console.log('gulp is running..');
+});
+ 
+gulp.task('css', async function () {
+  gulp.src('./css/*.css')
+    .pipe(uglifycss({
+      "maxLineLen": 80,
+      "uglyComments": true
+    }))
+    .pipe(gulp.dest('./dist/'));
+});
+
+//  gulp.task('run',['scss','css']);
+
+// gulp.task('watch',  function(){
+//   gulp.watch('sass/*.scss',['scss']); 
+//   gulp.watch('css/*.css',['css']);
+// });
+
+// gulp.task('default',['run','watch']);
+// gulp.task('watch',function(){
+//     gulp.watch('sass/*scss',gulp.series('sass'));
+// });
